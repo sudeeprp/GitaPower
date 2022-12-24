19e76f6ca9c918adfb94f076f64e86edd007e4f0
Sudeep Prasad <sudeep.rp@gmail.com>
2022-12-24 21:20:21 +0530

    red font size

--- feeder/lib/content_widget.dart
+++ feeder/lib/content_widget.dart
@@ -7,11 +7,29 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:askys/choice_selector.dart';
 import 'package:markdown/markdown.dart' as md;
+import 'package:stack/stack.dart' as stack;
 
-class MarkdownParser implements md.NodeVisitor {
-  final List<Widget> Function(String) _widgetMaker;
+enum SectionType {
+  shlokaNumber,
+  shlokaSA,
+  shlokaSAHK,
+  meaning,
+  commentary
+}
+
+class Section {
+  Section(this.sectionType, this.content);
+  SectionType sectionType;
+  Widget content;
+}
+
+class WidgetMaker implements md.NodeVisitor {
+  final List<TextSpan> Function(String text, String tag, String? elmclass) _inlineMaker;
+  final List<Widget> Function(List<TextSpan>, md.Element) _widgetMaker;
+  stack.Stack<md.Element> elementForCurrentText = stack.Stack();
   List<Widget> collectedWidgets = [];
-  MarkdownParser(this._widgetMaker);
+  List<TextSpan> collectedElements = [];
+  WidgetMaker(this._widgetMaker, this._inlineMaker);
 
   List<Widget> parse(String markdownContent) {
     List<String> lines = markdownContent.split('\n');
@@ -24,50 +42,114 @@ class MarkdownParser implements md.NodeVisitor {
 
   @override
   void visitElementAfter(md.Element element) {
-    // TODO: implement visitElementAfter
+    const widgetSeparators = ['h2', 'p', 'pre'];
+    if (widgetSeparators.contains(element.tag)) {
+      collectedWidgets.addAll(_widgetMaker(collectedElements, element));
+      collectedElements = [];
+    }
+    elementForCurrentText.pop();
   }
 
   @override
   bool visitElementBefore(md.Element element) {
+    elementForCurrentText.push(element);
     return true;
   }
 
   @override
-  void visitText(md.Text text) {
-    collectedWidgets.addAll(_widgetMaker(text.textContent));
+  void visitText(md.Text markdownText) {
+    final element = elementForCurrentText.top();
+    final processedText = _textForElement(markdownText.textContent, element);
+    collectedElements.addAll(_inlineMaker(processedText, element.tag, element.attributes['class']));
+  }
+
+  String _removeLeadingNewline(String wsCleaned) {
+    if (wsCleaned[0] == "\n") {
+      return wsCleaned.substring(1);
+    } else {
+      return wsCleaned;
+    }
+  }
+  String _textForElement(String inputText, md.Element element) {
+    if (element.tag == 'code') {
+      return inputText.trim();
+    } else {
+      return _removeLeadingNewline(inputText).replaceAll(RegExp(r"\s+"), " ");
+    }
   }
 }
 
-List<Widget> mdToWidgets(String markdown, List<Widget> Function(String) widgetMaker) {
-  return MarkdownParser(widgetMaker).parse(markdown);
+Text _spansToText(List<TextSpan> spans) {
+  if (spans.isEmpty) {
+    return const Text('');
+  } else if (spans.length == 1) {
+    return Text.rich(spans[0]);
+  } else {
+    return Text.rich(TextSpan(children: spans));
+  }
+}
+
+TextStyle? _styleFor(String tag, String? elmclass) {
+  if (elmclass == 'language-shloka-sa') {
+    return const TextStyle(color: Colors.red, fontSize: 20);
+  } else if (tag == 'code') {
+    return GoogleFonts.robotoMono(color: Colors.red);
+  } else {
+    return null;
+  }
+}
+List<TextSpan> formatMaker(String content, String tag, String? elmclass) {
+  return [TextSpan(text: content, style: _styleFor(tag, elmclass))];
+}
+
+bool _isVisible(md.Element element) {
+  Choices choice = Get.find();
+  final elmclass = element.attributes['class'];
+  if (elmclass == 'language-shloka-sa') {
+    return choice.script.value == ScriptPreference.devanagari;
+  } else if (elmclass == 'language-shloka-sa-hk') {
+    return choice.script.value == ScriptPreference.sahk;
+  }
+  return true;
+}
+
+List<Widget> textRichMaker(List<TextSpan> spans, md.Element element) {
+  return [Obx(()=> Visibility(
+       visible: _isVisible(element),
+       child: Padding(padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5), child: _spansToText(spans))
+       ))];
 }
 
 List<Widget> samplemdToWidgets(String markdown, BuildContext context) {
-  Choices choice = Get.find();
-  final codeStyle = GoogleFonts.robotoMono();
-  return [
-    Text.rich(TextSpan(text: '2-54', style: Theme.of(context).textTheme.headline2)),
-    Obx(()=> Visibility(
-      visible: choice.script.value == ScriptPreference.devanagari,
-      child: Text.rich(TextSpan(text: '''
-अर्जुन उवाच -
-स्थितप्रज्ञस्य का भाषा समाधिस्थस्य केशव ।
-स्थितधीः किम् प्रभाषेत किमासीत व्रजेत किम् ॥ ५४ ॥
-''', style: codeStyle)))),
-    Obx(()=> Visibility(
-      visible: choice.script.value == ScriptPreference.sahk,
-      child: Text.rich(TextSpan(text: '''
-arjuna uvAca -
-sthitaprajJasya kA bhASA samAdhisthasya kezava |
-sthitadhIH kim prabhASeta kimAsIta vrajeta kim || 54 ||
-''', style: codeStyle)))),
-    Obx(() => Text.rich(TextSpan(children: [
-      if (choice.isDevanagari()) TextSpan(text: 'अर्जुन उवाच ', style: codeStyle),
-      if (choice.isSAHK()) TextSpan(text: '[arjuna uvAca] ', style: codeStyle),
-      TextSpan(text: 'Arjuna said- '),
-    ])))
-  ];
+  return WidgetMaker(textRichMaker, formatMaker).parse(markdown);
 }
+
+// List<Widget> samplemdToWidgets(String markdown, BuildContext context) {
+//   Choices choice = Get.find();
+//   final codeStyle = GoogleFonts.robotoMono();
+//   return [
+//     Text.rich(TextSpan(text: '2-54', style: Theme.of(context).textTheme.headline2)),
+//     Obx(()=> Visibility(
+//       visible: choice.script.value == ScriptPreference.devanagari,
+//       child: Text.rich(TextSpan(text: '''
+// अर्जुन उवाच -
+// स्थितप्रज्ञस्य का भाषा समाधिस्थस्य केशव ।
+// स्थितधीः किम् प्रभाषेत किमासीत व्रजेत किम् ॥ ५४ ॥
+// ''', style: codeStyle)))),
+//     Obx(()=> Visibility(
+//       visible: choice.script.value == ScriptPreference.sahk,
+//       child: Text.rich(TextSpan(text: '''
+// arjuna uvAca -
+// sthitaprajJasya kA bhASA samAdhisthasya kezava |
+// sthitadhIH kim prabhASeta kimAsIta vrajeta kim || 54 ||
+// ''', style: codeStyle)))),
+//     Obx(() => Text.rich(TextSpan(children: [
+//       if (choice.isDevanagari()) TextSpan(text: 'अर्जुन उवाच ', style: codeStyle),
+//       if (choice.isSAHK()) TextSpan(text: '[arjuna uvAca] ', style: codeStyle),
+//       TextSpan(text: 'Arjuna said- '),
+//     ])))
+//   ];
+// }
 
 class ContentWidget extends StatelessWidget {
   const ContentWidget({Key? key}) : super(key: key);
--- feeder/test/content_test.dart
+++ feeder/test/content_test.dart
@@ -3,29 +3,64 @@ import 'package:askys/choice_selector.dart';
 import 'package:askys/content_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:markdown/markdown.dart' as md;
 
-List<Widget> oneTextMaker(String markdown)=> [Text(markdown)];
-  
+List<TextSpan> oneTextMaker(String content, String tag, String? elmclass)=> [TextSpan(text: content)];
+List<Widget> textRichMaker(List<TextSpan> spans, md.Element element) {
+  if (spans.isEmpty) {
+    return [];
+  } else if (spans.length == 1) {
+    return [Text.rich(spans[0])];
+  } else {
+    return [Text.rich(TextSpan(children: spans))];
+  }
+}
+
 void main() {
-  testWidgets('Renders a single markdown line as a RichText',
+  testWidgets('Renders a plain-text markdown line together',
       (WidgetTester tester) async {
     // Get.put(Choices());
     // final BuildContext context = tester.element(find.byType(Container));
-    final widgetWithOneMD = mdToWidgets('one two three', oneTextMaker);
+    final widgetWithOneMD = WidgetMaker(textRichMaker, oneTextMaker).parse('one two three');
     expect(widgetWithOneMD.length, equals(1));
     await tester
         .pumpWidget(GetMaterialApp(home: Column(children: widgetWithOneMD)));
     expect(find.text('one two three'), findsOneWidget);
   });
-  testWidgets('Separates markdown with inline code', (WidgetTester tester) async {
+  testWidgets('Text with inline code remains inline in one widget', (WidgetTester tester) async {
     List<String> mdTexts = [];
-    List<Widget> mdTextCollector(String text) {
-      mdTexts.add(text);
-      return [Text(text)];
+    List<TextSpan> mdTextCollector(String content, String tag, String? elmclass) {
+      mdTexts.add(content);
+      return [TextSpan(text: content)];
     }
-    final widgetsWithInlineCode = mdToWidgets('inline `code`', mdTextCollector);
-    expect(widgetsWithInlineCode.length, equals(1)); // Needs to be one span with two nested spans
+    final widgetsWithInlineCode = WidgetMaker(textRichMaker, mdTextCollector).parse('inline `code`');
+    expect(widgetsWithInlineCode.length, equals(1));
     expect(mdTexts[0], equals('inline '));
     expect(mdTexts[1], equals('code'));
+  });
+  testWidgets('Multiline code is in separate widget', (WidgetTester tester) async {
+    final widgetsWithMultilineCode = WidgetMaker(textRichMaker, oneTextMaker).parse('''
+## 2-54
+```shloka-sa
+अर्जुन उवाच -
+स्थितप्रज्ञस्य 
+```
+''');
+    expect(widgetsWithMultilineCode.length, equals(2));
+  });
+  testWidgets('Single newline appears as whitespace', (WidgetTester tester) async {
+    final widgetsForSingleNewline = WidgetMaker(textRichMaker, oneTextMaker).parse('''
+To describe someone standing 
+[firm in wisdom](sthitaprajna_xlat)
+''');
+    expect(widgetsForSingleNewline.length, equals(1));
+  });
+  testWidgets('Double newline makes a new row', (WidgetTester tester) async {
+    final widgetsForTwoLines = WidgetMaker(textRichMaker, oneTextMaker).parse('''
+how does he behave?
+
+While describing this
+''');
+    expect(widgetsForTwoLines.length, equals(2));
   });
 }
