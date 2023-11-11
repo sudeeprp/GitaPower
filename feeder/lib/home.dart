b9d2dc316b39981c98e28fffb75787891bdb938a
Sudeep Prasad <sudeep.rp@gmail.com>
2023-11-11 19:03:54 +0530

    unify screenify

--- feeder/lib/home.dart
+++ feeder/lib/home.dart
@@ -2,35 +2,12 @@ import 'package:askys/chapter_shloka_widget.dart';
 import 'package:askys/notes_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:askys/choice_selector.dart';
 import 'package:askys/choice_bindings.dart';
 import 'package:askys/content_widget.dart';
 import 'package:askys/begin_widget.dart';
 import 'package:askys/chapters_widget.dart';
 import 'package:askys/feed_widget.dart';
-
-Widget screenify(Widget body, {AppBar? appBar}) {
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-  return Scaffold(
-      key: scaffoldKey,
-      appBar: appBar,
-      endDrawer: SafeArea(
-          child: Drawer(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [themeSelector(), scriptSelector(), headerSelector()]))),
-      body: SafeArea(
-        child: Stack(children: [
-          body,
-          Positioned(
-              bottom: 0,
-              right: 0,
-              child: GestureDetector(
-                  onTap: () => scaffoldKey.currentState?.openEndDrawer(),
-                  child: const Icon(key: Key('home/settingsicon'), Icons.settings))),
-        ]),
-      ));
-}
+import 'package:askys/screenify.dart';
 
 Widget makeMyHome() {
   return GetMaterialApp(
