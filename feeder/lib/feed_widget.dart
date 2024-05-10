458ad354b3579bdc34780b172d574a91daacc8b0
Sudeep Prasad <sudeep.rp@gmail.com>
2024-05-10 23:06:06 +0530

    hide opener on tap

--- feeder/lib/feed_widget.dart
+++ feeder/lib/feed_widget.dart
@@ -4,31 +4,38 @@ import 'package:get/get.dart';
 import 'feedcontent.dart';
 
 Widget contentWithOpenerPane(String filename, int count) {
+  final FeedContent feedContent = Get.find();
+  void hideOpener() {
+    feedContent.openerCovers[count - 1].value = false;
+  }
   return Obx(() {
-    final FeedContent feedContent = Get.find();
     if (feedContent.openerCovers[count - 1].value) {
       return Stack(
         children: [
           buildContentFeed(filename, key: Key('feed/$count')),
           Dismissible(
               key: Key('overq/$count'),
-              onDismissed: (direction) {
-                feedContent.openerCovers[count - 1].value = false;
-              },
-              child: Container(
-                margin: const EdgeInsets.fromLTRB(15, 5, 0, 8),
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.purple.withOpacity(0.75), Colors.purple.withOpacity(0.95)],
-                      begin: AlignmentDirectional.topStart,
-                      end: AlignmentDirectional.bottomEnd,
-                    ),
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(10), bottomLeft: Radius.circular(10))),
-                constraints: const BoxConstraints.expand(),
-                child: Center(
-                    child: Text(feedContent.openerQs[count - 1],
-                        textAlign: TextAlign.center, style: const TextStyle(fontSize: 32))),
+              onDismissed: (direction) => hideOpener(),
+              child: GestureDetector(
+                onTap: hideOpener,
+                  child: Container(
+                  margin: const EdgeInsets.fromLTRB(15, 5, 0, 8),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.deepOrange.withOpacity(0.75),
+                          Colors.purple.withOpacity(0.95)
+                        ],
+                        begin: AlignmentDirectional.topStart,
+                        end: AlignmentDirectional.bottomEnd,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10), bottomLeft: Radius.circular(10))),
+                  constraints: const BoxConstraints.expand(),
+                  child: Center(
+                      child: Text(feedContent.openerQs[count - 1],
+                          textAlign: TextAlign.center, style: const TextStyle(fontSize: 32))),
+                )
               )),
         ],
       );
