17dcbdac2315e0d7e2a5c7a598320122e542eb0c
Sudeep Prasad <sudeep.rp@gmail.com>
2024-05-26 11:51:41 +0530

    include drop_cap

--- feeder/lib/content_widget.dart
+++ feeder/lib/content_widget.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'package:askys/mdcontent.dart';
 import 'package:askys/content_actions.dart';
-import 'package:drop_cap_text/drop_cap_text.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
