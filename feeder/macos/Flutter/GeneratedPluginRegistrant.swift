2bb2f570faf49b06935c97207c92f30c31840231
Sudeep Prasad <sudeep.rp@gmail.com>
2024-08-21 08:23:21 +0530

    plugin at start

--- feeder/macos/Flutter/GeneratedPluginRegistrant.swift
+++ feeder/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import app_links
-import audio_session
 import firebase_core
 import firebase_crashlytics
-import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
-import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
