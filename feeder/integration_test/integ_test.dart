aca01b8cbfa1d703b863b6cd4b68da12a3132a5e
Sudeep Prasad <sudeep.rp@gmail.com>
2024-11-24 21:57:25 +0530

    home and tour, light and dark

--- feeder/integration_test/integ_test.dart
+++ feeder/integration_test/integ_test.dart
@@ -1,19 +1,34 @@
+import 'package:askys/choice_selector.dart';
 import 'package:askys/home.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding();
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  Future<void> screenshot(String name) async {
+    await binding.convertFlutterSurfaceToImage();
+    await binding.takeScreenshot(name);
+  }
+
+  Future<void> captureScreens(WidgetTester tester, String theme) async {
+    await tester.pumpAndSettle();
+    await screenshot('home-$theme');
+    await Get.offNamed('/tour');
+    await tester.pumpAndSettle();
+    await screenshot('tour-$theme');
+  }
+
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter', (tester) async {
-      // Load app widget.
+    testWidgets('capture screens', (tester) async {
       await tester.pumpWidget(makeMyHome());
-      await tester.pumpAndSettle();
-      await binding.convertFlutterSurfaceToImage();
-      await tester.pumpAndSettle();
-      await binding.takeScreenshot('test-screenshot');
+      Choices choices = Get.find();
+      choices.theme.value = ReadingTheme.light;
+      await captureScreens(tester, 'light');
+      choices.theme.value = ReadingTheme.dark;
+      await captureScreens(tester, 'dark');
     });
   });
 }
