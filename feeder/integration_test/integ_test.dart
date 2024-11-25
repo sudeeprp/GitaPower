d2a4969fd6929740764e84e20467c8e42f3fb085
Sudeep Prasad <sudeep.rp@gmail.com>
2024-11-25 22:55:14 +0530

    all screens light and dark

--- feeder/integration_test/integ_test.dart
+++ feeder/integration_test/integ_test.dart
@@ -10,23 +10,44 @@ void main() {
 
   group('end-to-end test', () {
     for (final theme in [ReadingTheme.light, ReadingTheme.dark]) {
-      Future<void> screenIt(WidgetTester tester, String screenName) async {
-        await tester.pumpWidget(makeMyHome());
-        await tester.pumpAndSettle();
-        if (screenName != 'home') {
-          Get.offNamed(screenName);
-        }
+      Future<void> screenIt(WidgetTester tester, String screenName, {String? filename}) async {
         Choices choices = Get.find();
-
         choices.theme.value = theme;
         await tester.pumpAndSettle();
 
         await binding.convertFlutterSurfaceToImage();
         await tester.pumpAndSettle();
-        await binding.takeScreenshot(screenshotFilename(screenName, theme));
+        filename ??= screenshotFilename(screenName, theme);
+        await binding.takeScreenshot(filename);
       }
-      for (final screen in ['home', '/tour', '/feed', '/shlokaheaders/Chapter_4.md', '/shloka/4-4.md']) {
+
+      testWidgets('home screenshot - ${theme.toString()}', (tester) async {
+        await tester.pumpWidget(makeMyHome());
+        await screenIt(tester, 'home');
+      });
+      testWidgets('browse chapters screenshot - ${theme.toString()}', (tester) async {
+        await tester.pumpWidget(makeMyHome());
+        Choices choices = Get.find();
+        choices.browsingPreference.value = BrowsingPreference.chapters;
+        await tester.pumpAndSettle();
+        Get.offNamed('/browse');
+        await tester.pumpAndSettle();
+        await screenIt(tester, 'browse-chapters');
+      });
+      testWidgets('browse notes screenshot - ${theme.toString()}', (tester) async {
+        await tester.pumpWidget(makeMyHome());
+        Choices choices = Get.find();
+        choices.browsingPreference.value = BrowsingPreference.notes;
+        await tester.pumpAndSettle();
+        Get.offNamed('/browse');
+        await tester.pumpAndSettle();
+        await screenIt(tester, 'browse-notes');
+      });
+      for (final screen in ['/tour', '/feed', '/shlokaheaders/Chapter_4.md', '/shloka/4-4.md']) {
         testWidgets('$screen screenshot - ${theme.toString()}', (tester) async {
+          await tester.pumpWidget(makeMyHome());
+          Get.offNamed(screen);
+          await tester.pumpAndSettle();
           await screenIt(tester, screen);
         });
       }
@@ -35,6 +56,6 @@ void main() {
 }
 
 String screenshotFilename(String screenName, ReadingTheme theme) {
-  return
-   '${screenName.replaceFirst('/', '').replaceAll(RegExp(r'/.*$'), '')}-${theme.toString().replaceAll('ReadingTheme.', '')}';
+  String themeName = theme.toString().replaceAll('ReadingTheme.', '');
+  return '${screenName.replaceFirst('/', '').replaceAll(RegExp(r'/.*$'), '')}-$themeName';
 }
