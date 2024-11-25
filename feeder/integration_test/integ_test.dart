bc1199e220be8bb87b2ca84c6c906e705bf14dee
Sudeep Prasad <sudeep.rp@gmail.com>
2024-11-25 08:29:48 +0530

    Update integ_test.dart

--- feeder/integration_test/integ_test.dart
+++ feeder/integration_test/integ_test.dart
@@ -1,6 +1,6 @@
 // import 'package:askys/choice_selector.dart';
 import 'package:askys/home.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 // import 'package:get/get.dart';
 import 'package:integration_test/integration_test.dart';
 
