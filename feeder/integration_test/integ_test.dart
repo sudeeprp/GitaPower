493e3ab8007ec9bb7894e738147b97e4ced2997b
Sudeep Prasad <sudeep.rp@gmail.com>
2024-11-25 22:23:03 +0530

    one screen per test

--- feeder/integration_test/integ_test.dart
+++ feeder/integration_test/integ_test.dart
@@ -1,19 +1,40 @@
-import 'package:askys/home.dart';
+import 'package:get/get.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:askys/home.dart';
+import 'package:askys/choice_selector.dart';
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding();
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter', (tester) async {
-      // Load app widget.
-      await tester.pumpWidget(makeMyHome());
-      await tester.pumpAndSettle();
-      await binding.convertFlutterSurfaceToImage();
-      await tester.pumpAndSettle();
-      await binding.takeScreenshot('test-screenshot');
-    });
+    for (final theme in [ReadingTheme.light, ReadingTheme.dark]) {
+      Future<void> screenIt(WidgetTester tester, String screenName) async {
+        await tester.pumpWidget(makeMyHome());
+        await tester.pumpAndSettle();
+        if (screenName != 'home') {
+          Get.offNamed(screenName);
+        }
+        Choices choices = Get.find();
+
+        choices.theme.value = theme;
+        await tester.pumpAndSettle();
+
+        await binding.convertFlutterSurfaceToImage();
+        await tester.pumpAndSettle();
+        await binding.takeScreenshot(screenshotFilename(screenName, theme));
+      }
+      for (final screen in ['home', '/tour', '/feed', '/shlokaheaders/Chapter_4.md', '/shloka/4-4.md']) {
+        testWidgets('$screen screenshot - ${theme.toString()}', (tester) async {
+          await screenIt(tester, screen);
+        });
+      }
+    }
   });
+}
+
+String screenshotFilename(String screenName, ReadingTheme theme) {
+  return
+   '${screenName.replaceFirst('/', '').replaceAll(RegExp(r'/.*$'), '')}-${theme.toString().replaceAll('ReadingTheme.', '')}';
 }
