e721961cd6940504dcf427d8a0ec1f6377f858eb
Sudeep Prasad <sudeep.rp@gmail.com>
2024-11-25 06:50:08 +0530

    Roll back for hang

--- feeder/integration_test/integ_test.dart
+++ feeder/integration_test/integ_test.dart
@@ -1,7 +1,7 @@
-import 'package:askys/choice_selector.dart';
+// import 'package:askys/choice_selector.dart';
 import 'package:askys/home.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:get/get.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
@@ -16,19 +16,19 @@ void main() {
   Future<void> captureScreens(WidgetTester tester, String theme) async {
     await tester.pumpAndSettle();
     await screenshot('home-$theme');
-    await Get.offNamed('/tour');
-    await tester.pumpAndSettle();
-    await screenshot('tour-$theme');
+    // await Get.offNamed('/tour');
+    // await tester.pumpAndSettle();
+    // await screenshot('tour-$theme');
   }
 
   group('end-to-end test', () {
     testWidgets('capture screens', (tester) async {
       await tester.pumpWidget(makeMyHome());
-      Choices choices = Get.find();
-      choices.theme.value = ReadingTheme.light;
+      // Choices choices = Get.find();
+      // choices.theme.value = ReadingTheme.light;
       await captureScreens(tester, 'light');
-      choices.theme.value = ReadingTheme.dark;
-      await captureScreens(tester, 'dark');
+      // choices.theme.value = ReadingTheme.dark;
+      // await captureScreens(tester, 'dark');
     });
   });
 }
