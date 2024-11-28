19b419e41695aa1e711a58eef5a328504ff97da1
Sudeep Prasad <sudeep.rp@gmail.com>
2024-11-28 17:50:15 +0530

    screenshots to fastlane

---
+++ feeder/screenshots-to-fastlane.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+PHONE_SCREENS_FOLDER=./android/fastlane/metadata/android/en-US/images/phoneScreenshots
+rm $PHONE_SCREENS_FOLDER/*
+cp ./screenshots/home-light.png $PHONE_SCREENS_FOLDER/1_en-US.png
+cp ./screenshots/browse-chapters-light.png $PHONE_SCREENS_FOLDER/2_en-US.png
+cp ./screenshots/shlokaheaders-light.png $PHONE_SCREENS_FOLDER/3_en-US.png
+cp ./screenshots/browse-notes-light.png $PHONE_SCREENS_FOLDER/4_en-US.png
+cp ./screenshots/shloka-light.png $PHONE_SCREENS_FOLDER/5_en-US.png
+cp ./screenshots/shloka-dark.png $PHONE_SCREENS_FOLDER/6_en-US.png
+cp ./screenshots/feed-light.png $PHONE_SCREENS_FOLDER/7_en-US.png
+cp ./screenshots/tour-light.png  $PHONE_SCREENS_FOLDER/8_en-US.png
