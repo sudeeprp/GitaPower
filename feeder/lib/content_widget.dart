563bda789c35937607ecc30b669207b9e9477edb
Sudeep <sudeep.rp@gmail.com>
2025-10-26 23:25:23 +0530

    format

--- feeder/lib/content_widget.dart
+++ feeder/lib/content_widget.dart
@@ -514,28 +514,34 @@ class ContentWidget extends StatelessWidget {
         if (contentNotes.notesLoaded.value) {
           final preNote = contentNotes.noteForMD(mdFilename);
           return Row(children: [
-            Expanded(flex: 1, child: GestureDetector(
-              onTap: () => Get.back(),
-              child: Icon(
-              Icons.arrow_back,
-              color: Colors.purple.withValues(alpha: 0.7),
-              size: 48,
-              ),
-            )),
-            Expanded(flex: 9, child: _buildNote(
-              context,
-              IntrinsicHeight(
-                  child: Row(children: [
-                Expanded(
-                    flex: 17,
-                    child: Text.rich(TextSpan(text: toPlainText(preNote ?? '')), style: styleFor('note')?.copyWith(fontSize: 10))),
-                const VerticalDivider(thickness: 1, indent: 5, endIndent: 5, color: Colors.grey),
-                Expanded(
-                  flex: 3,
-                  child: Text(Chapter.filenameToShortTitle(mdFilename),
-                      style: Theme.of(context).textTheme.bodySmall),
-                )
-              ]))))]);
+            Expanded(
+                flex: 1,
+                child: GestureDetector(
+                  onTap: () => Get.back(),
+                  child: Icon(
+                    Icons.arrow_back,
+                    color: Colors.purple.withValues(alpha: 0.7),
+                    size: 48,
+                  ),
+                )),
+            Expanded(
+                flex: 9,
+                child: _buildNote(
+                    context,
+                    IntrinsicHeight(
+                        child: Row(children: [
+                      Expanded(
+                          flex: 17,
+                          child: Text.rich(TextSpan(text: toPlainText(preNote ?? '')),
+                              style: styleFor('note')?.copyWith(fontSize: 10))),
+                      const VerticalDivider(thickness: 1, indent: 5, endIndent: 5, color: Colors.grey),
+                      Expanded(
+                        flex: 3,
+                        child: Text(Chapter.filenameToShortTitle(mdFilename),
+                            style: Theme.of(context).textTheme.bodySmall),
+                      )
+                    ]))))
+          ]);
         } else {
           return const SizedBox.shrink();
         }
