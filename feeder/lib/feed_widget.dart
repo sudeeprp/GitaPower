fb5fba02a4bfe87a2ef731e994aa0bba725c238f
Sudeep <sudeep.rp@gmail.com>
2025-11-16 12:35:49 +0530

    try image not loading

--- feeder/lib/feed_widget.dart
+++ feeder/lib/feed_widget.dart
@@ -31,7 +31,7 @@ class ShlokaInsideFeed extends StatelessWidget {
               Padding(
                   padding: EdgeInsets.symmetric(horizontal: 4),
                   child: Image.asset(
-                    'images/chapter_$chapterNumber.png',
+                    'images/one-step.png',
                     width: 28,
                     height: 28,
                     // errorBuilder: (context, error, stackTrace) {
